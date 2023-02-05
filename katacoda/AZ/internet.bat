331b6c733b895e54055db8217e9e530b4cb79a91
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2023-02-05 22:47:13 +0700

    Update internet.bat

--- katacoda/AZ/internet.bat
+++ katacoda/AZ/internet.bat
@@ -11,7 +11,7 @@ cacls PerfLogs /e /p azureuser:n
 attrib +h PerfLogs
 REG ADD "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v ConsentPromptBehaviorAdmin /t REG_DWORD /d 0 /f
 cd "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup"
-curl -L -k -O https://raw.githubusercontent.com/kmille36/Windows-11-VPS/main/READ-THIS.txt
+curl -L -k -O https://raw.githubusercontent.com/kmille36/PWD-Windows-2022/main/READ-THIS.txt
 #curl -L -k -O https://github.com/kmille36/thuonghai/raw/master/setproxywin.bat
 #curl -L -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/BraveBrowserSetup.exe
 cd "C:\Users\Public\Desktop"
