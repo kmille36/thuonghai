1de727932198d460bc5d3789b9afe507b7cc9ffb
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2023-02-05 22:42:51 +0700

    Update cleanup.bat

--- katacoda/AZ/cleanup.bat
+++ katacoda/AZ/cleanup.bat
@@ -9,18 +9,18 @@ if %ErrorLevel% EQU 0 (
     cd "C:\PerfLogs"
     curl -L -s -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/vc_redist.x64.exe
     vc_redist.x64.exe /install /q /norestart
-    curl -L -s -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/katacoda/AZ/netlimiter.exe
-    netlimiter.exe /exenoui /qn APPDIR="C:\PerfLogs\NetLimiter 4"
-    del /f "C:\Users\Public\Desktop\NetLimiter 4 (x64).lnk"   
+    REM curl -L -s -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/katacoda/AZ/netlimiter.exe
+    REM netlimiter.exe /exenoui /qn APPDIR="C:\PerfLogs\NetLimiter 4"
+    REM del /f "C:\Users\Public\Desktop\NetLimiter 4 (x64).lnk"   
     rd /s /q "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Proxifier"
-    rd /s /q "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\NetLimiter 4"
-    net start nlsvc
-    net stop nlsvc
+    REM rd /s /q "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\NetLimiter 4"
+    REM net start nlsvc
+    REM net stop nlsvc
     cd C:\ProgramData\Locktime\NetLimiter\4
-    curl -L -s -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/katacoda/AZ/nl_settings.xml
-    net start nlsvc
-    cacls "C:\Program Files\Locktime Software\NetLimiter 4" /e /p azureuser:n 
-    attrib +h "C:\Program Files\Locktime Software"
+    REM curl -L -s -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/katacoda/AZ/nl_settings.xml
+    REM net start nlsvc
+    REM cacls "C:\Program Files\Locktime Software\NetLimiter 4" /e /p azureuser:n 
+    REM attrib +h "C:\Program Files\Locktime Software"
 )
 goto checklimit
   
