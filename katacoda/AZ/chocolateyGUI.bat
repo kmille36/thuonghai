45ee60b372956361c457a08a0dcf4533f57f685b
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2023-02-05 21:44:20 +0700

    Delete katacoda/AZ directory

--- katacoda/AZ/chocolateyGUI.bat
+++
@@ -1,6 +0,0 @@
-:install
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
-choco install chocolateygui --params="'/ShowConsoleOutput=True /OutdatedPackagesCacheDurationInMinutes=120 /Global'" -y
-if not exist "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Chocolatey GUI.lnk" goto install
-copy "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Chocolatey GUI.lnk" "C:\Users\Public\Desktop"
-exit
