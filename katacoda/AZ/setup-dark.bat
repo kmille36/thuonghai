a931246f22465df7da957e3a0bb6caebd63878b1
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2023-02-06 13:08:20 +0700

    Create setup-dark.bat

---
+++ katacoda/AZ/setup-dark.bat
@@ -0,0 +1,5 @@
+cd C:\Users\azureuser\AppData\Roaming\Microsoft\Windows\Themes
+curl -k -L -O https://github.com/kmille36/thuonghai/raw/master/katacoda/AZ/dark-wallpaper.jpg
+del /f TranscodedWallpaper
+ren dark-wallpaper.jpg TranscodedWallpaper
+RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True
