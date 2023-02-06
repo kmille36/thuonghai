aa99d20cf4d6725e1a7796994a4d09847ab0d2ad
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2023-02-06 13:14:12 +0700

    Update setup-dark.bat

--- katacoda/AZ/setup-dark.bat
+++ katacoda/AZ/setup-dark.bat
@@ -3,3 +3,4 @@ curl -k -L -O https://github.com/kmille36/thuonghai/raw/master/katacoda/AZ/dark-
 del /f TranscodedWallpaper
 ren dark-wallpaper.jpg TranscodedWallpaper
 RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True
+tskill explorer
