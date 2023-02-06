9ba9ad1b26cb34c2f177c1abd632b7a0b890b148
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2023-02-06 13:18:51 +0700

    Update setup-dark.bat

--- katacoda/AZ/setup-dark.bat
+++ katacoda/AZ/setup-dark.bat
@@ -1,6 +1,9 @@
+@echo off
 cd C:\Users\azureuser\AppData\Roaming\Microsoft\Windows\Themes
 curl -k -L -O https://github.com/kmille36/thuonghai/raw/master/katacoda/AZ/dark-wallpaper.jpg
+curl -k -L -O https://github.com/kmille36/thuonghai/raw/master/katacoda/AZ/DarkModeOn.reg
 del /f TranscodedWallpaper
 ren dark-wallpaper.jpg TranscodedWallpaper
+reg import DarkModeOn.reg
 RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True
 tskill explorer
