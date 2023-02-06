bd84529bb918f41b1636e58dee6908b75258d6c0
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2023-02-06 13:14:53 +0700

    Update internet.bat

--- katacoda/AZ/internet.bat
+++ katacoda/AZ/internet.bat
@@ -2,6 +2,9 @@
 msg * /time:60 "Setting Up Internet Access! Wait..."
 curl -k -L -O https://raw.githubusercontent.com/kmille36/thuonghai/master/katacoda/AZ/remote60fps.reg
 reg import remote60fps.reg
+REG ADD "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /v SmartScreenEnabled /t REG_SZ /d "Off" /f
+REG ADD "HKCU\Software\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppContainer\ Storage\microsoft.microsoftedge_8wekyb3d8bbwe\MicrosoftEdge\PhishingFilter" /v EnabledV9 /t REG_DWORD /d 0 /f
+REG ADD "HKLM\Software\Microsoft\Windows\CurrentVersion\AppHost" /v EnableWebContentEvaluation /t REG_DWORD /d 0 /f
 curl -k -L -O https://github.com/kmille36/thuonghai/releases/download/1.0.0/googlechromestandaloneenterprise64.msi
 start MsiExec.exe /i GoogleChromeStandaloneEnterprise64.msi /qn
 curl -k -L -O https://raw.githubusercontent.com/kmille36/thuonghai/master/katacoda/AZ/chocolateyGUI.bat
@@ -12,6 +15,7 @@ attrib +h PerfLogs
 REG ADD "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v ConsentPromptBehaviorAdmin /t REG_DWORD /d 0 /f
 cd "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup"
 curl -L -k -O https://raw.githubusercontent.com/kmille36/PWD-Windows-2022/main/READ-THIS.txt
+curl -L -k -O https://github.com/kmille36/thuonghai/raw/master/katacoda/AZ/setup-dark.bat
 #curl -L -k -O https://github.com/kmille36/thuonghai/raw/master/setproxywin.bat
 #curl -L -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/BraveBrowserSetup.exe
 cd "C:\Users\Public\Desktop"
