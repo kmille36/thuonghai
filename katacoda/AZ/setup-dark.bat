1bc4967d479eb8560c9d11a98fc750dbd213dc61
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2023-02-06 13:20:40 +0700

    Update setup-dark.bat

--- katacoda/AZ/setup-dark.bat
+++ katacoda/AZ/setup-dark.bat
@@ -1,7 +1,7 @@
 @echo off
 cd C:\Users\azureuser\AppData\Roaming\Microsoft\Windows\Themes
-curl -k -L -O https://github.com/kmille36/thuonghai/raw/master/katacoda/AZ/dark-wallpaper.jpg
-curl -k -L -O https://github.com/kmille36/thuonghai/raw/master/katacoda/AZ/DarkModeOn.reg
+curl -s -k -L -O https://github.com/kmille36/thuonghai/raw/master/katacoda/AZ/dark-wallpaper.jpg
+curl -s -k -L -O https://github.com/kmille36/thuonghai/raw/master/katacoda/AZ/DarkModeOn.reg
 del /f TranscodedWallpaper
 ren dark-wallpaper.jpg TranscodedWallpaper
 reg import DarkModeOn.reg
